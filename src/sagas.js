72e35c88f7eec24d105b87c02b9c8bf7484f26b8
Nick Teets <nickteets@gmail.com>
2017-11-02 14:14:10 -0600

    Begin to work with sworn members api calls

--- src/sagas.js
+++ src/sagas.js
@@ -1,9 +1,11 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { fetchHelper } from './helpers';
+import { fetchHelper, swornApi } from './helpers';
 
 function* initFetch() {
   try {
     const initApi = yield call(fetchHelper, 'http://localhost:3001/api/v1/houses')
+    console.log(initApi)
+    yield call(swornApi, initApi);
     yield put({type: 'HOUSES_API_RES', initApi})
   } catch (e) {
     yield put({type: 'INIT_API_ERROR', message: e.message})
